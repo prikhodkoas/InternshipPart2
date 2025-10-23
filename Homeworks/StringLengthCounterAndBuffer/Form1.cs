3ad85243fdae13b5bfd6f31fba7e836b13ed710b
ValeryKuzh <valery.kuzh@gmail.com>
2025-10-23 14:00:07 +0300

    Add Documentation

--- Homeworks/StringLengthCounterAndBuffer/Form1.cs
+++ Homeworks/StringLengthCounterAndBuffer/Form1.cs
@@ -5,7 +5,11 @@ namespace StringLengthCounterAndBuffer
 {
     public partial class Form1 : Form
     {
-        private bool IsInitialised = true;
+        /// <summary>
+        /// Поле для проверки является ли запуск первым
+        /// </summary>
+        private bool _isInitialised = true;
+
         public Form1()
         {
             InitializeComponent();
@@ -13,23 +17,34 @@ namespace StringLengthCounterAndBuffer
             textBox2.Text = textBox1.Text.Length.ToString();
         }
 
+        /// <summary>
+        /// При изменении текста производится подсчет длины строки
+        /// </summary>
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
             textBox2.Text = textBox1.Text.Length.ToString(); 
         }
 
+        /// <summary>
+        /// При выходе из textbox текст заносится в буфер обмена и удаляется из textBox
+        /// </summary>
         private void textBox1_Leave(object sender, EventArgs e)
         {
             Clipboard.SetText(textBox1.Text);
             textBox1.Clear();
         }
 
+        /// <summary>
+        /// При повторном фокусе textBox текст копируется из буфера обмена в textBox 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void textBox1_Enter(object sender, EventArgs e)
         {
-            if (IsInitialised)
+            if (_isInitialised)
             {
-                IsInitialised = false;
+                _isInitialised = false;
                 return;
             }
             textBox1.Text = Clipboard.GetText();
