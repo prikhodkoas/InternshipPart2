4e68f63714ecfba1360b83116ade55e9a52a05a5
ValeryKuzh <valery.kuzh@gmail.com>
2025-10-24 14:14:31 +0300

    Add clearing clipboard before working

--- Homeworks/StringLengthCounterAndBuffer/Form1.cs
+++ Homeworks/StringLengthCounterAndBuffer/Form1.cs
@@ -26,7 +26,7 @@ namespace StringLengthCounterAndBuffer
         private void textBox1_Leave(object sender, EventArgs e)
         {
             if (!string.IsNullOrEmpty(textBox1.Text))
-            {
+            { 
                 Clipboard.SetText(textBox1.Text);
             }
             textBox1.Clear();
@@ -36,7 +36,7 @@ namespace StringLengthCounterAndBuffer
         /// При повторном фокусе textBox текст копируется из буфера обмена в textBox 
         /// </summary>
         private void textBox1_Enter(object sender, EventArgs e)
-        {
+        { 
             textBox1.Text = Clipboard.GetText();
         }
 
@@ -45,6 +45,7 @@ namespace StringLengthCounterAndBuffer
         /// </summary>
         private void Form1_Shown(object sender, EventArgs e)
         {
+            Clipboard.Clear();
             textBox1.Enter += textBox1_Enter;
             textBox1.Leave += textBox1_Leave;
         }
