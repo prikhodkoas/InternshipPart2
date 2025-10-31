96bf775d4f40fc5c4308e98352c9e03906d3f2b5
ValeryKuzh <valery.kuzh@gmail.com>
2025-10-31 12:34:23 +0300

    add  drag&drop for events

--- Homeworks/ScheduleEventCalendar/EventViewer.Designer.cs
+++ Homeworks/ScheduleEventCalendar/EventViewer.Designer.cs
@@ -37,25 +37,23 @@ namespace ScheduleEventCalendar
             // 
             this.LstBxEvents.Dock = System.Windows.Forms.DockStyle.Fill;
             this.LstBxEvents.FormattingEnabled = true;
-            this.LstBxEvents.ItemHeight = 20;
-            this.LstBxEvents.Location = new System.Drawing.Point(228, 0);
-            this.LstBxEvents.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.LstBxEvents.Location = new System.Drawing.Point(164, 0);
             this.LstBxEvents.Name = "LstBxEvents";
-            this.LstBxEvents.Size = new System.Drawing.Size(222, 249);
+            this.LstBxEvents.Size = new System.Drawing.Size(136, 162);
             this.LstBxEvents.TabIndex = 1;
             this.LstBxEvents.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LstBxEvents_MouseDown);
             // 
             // UpdateEventToolStripMenuItem
             // 
             this.UpdateEventToolStripMenuItem.Name = "UpdateEventToolStripMenuItem";
-            this.UpdateEventToolStripMenuItem.Size = new System.Drawing.Size(278, 32);
+            this.UpdateEventToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
             this.UpdateEventToolStripMenuItem.Text = "Редактировать событие";
             this.UpdateEventToolStripMenuItem.Click += new System.EventHandler(this.UpdateEventToolStripMenuItem_Click);
             // 
             // DeleteEventToolStripMenuItem
             // 
             this.DeleteEventToolStripMenuItem.Name = "DeleteEventToolStripMenuItem";
-            this.DeleteEventToolStripMenuItem.Size = new System.Drawing.Size(278, 32);
+            this.DeleteEventToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
             this.DeleteEventToolStripMenuItem.Text = "Удалить событие";
             // 
             // contextMenuStripEventList
@@ -65,12 +63,12 @@ namespace ScheduleEventCalendar
             this.UpdateEventToolStripMenuItem,
             this.DeleteEventToolStripMenuItem});
             this.contextMenuStripEventList.Name = "contextMenuStripEventList";
-            this.contextMenuStripEventList.Size = new System.Drawing.Size(279, 101);
+            this.contextMenuStripEventList.Size = new System.Drawing.Size(205, 48);
             // 
             // AddEventToolStripMenuItem
             // 
             this.AddEventToolStripMenuItem.Name = "AddEventToolStripMenuItem";
-            this.AddEventToolStripMenuItem.Size = new System.Drawing.Size(235, 32);
+            this.AddEventToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.AddEventToolStripMenuItem.Text = "Добавить событие";
             this.AddEventToolStripMenuItem.Click += new System.EventHandler(this.AddEventToolStripMenuItem_Click);
             // 
@@ -80,37 +78,36 @@ namespace ScheduleEventCalendar
             this.contextMenuStripCalendar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.AddEventToolStripMenuItem});
             this.contextMenuStripCalendar.Name = "contextMenuStripCalendar";
-            this.contextMenuStripCalendar.Size = new System.Drawing.Size(236, 36);
+            this.contextMenuStripCalendar.Size = new System.Drawing.Size(177, 26);
             // 
             // monthCalendar
             // 
             this.monthCalendar.Dock = System.Windows.Forms.DockStyle.Left;
             this.monthCalendar.Location = new System.Drawing.Point(0, 0);
+            this.monthCalendar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.monthCalendar.Name = "monthCalendar";
             this.monthCalendar.TabIndex = 0;
-            this.monthCalendar.DateChanged += monthCalendar_DateChanged;
+            this.monthCalendar.DateChanged += MonthCalendar_DateChanged;
             // 
             // EventViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.LstBxEvents);
             this.Controls.Add(this.monthCalendar);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MaximumSize = new System.Drawing.Size(0, 249);
-            this.MinimumSize = new System.Drawing.Size(450, 249);
+            this.MaximumSize = new System.Drawing.Size(0, 162);
+            this.MinimumSize = new System.Drawing.Size(300, 162);
             this.Name = "EventViewer";
-            this.Size = new System.Drawing.Size(450, 249);
+            this.Size = new System.Drawing.Size(300, 162);
             this.contextMenuStripEventList.ResumeLayout(false);
             this.contextMenuStripCalendar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
-
         #endregion
 
-        private System.Windows.Forms.ListBox LstBxEvents;
+        private ListBox LstBxEvents;
         private MyCalendar monthCalendar;
         private ToolStripMenuItem UpdateEventToolStripMenuItem;
         private ToolStripMenuItem DeleteEventToolStripMenuItem;
--- Homeworks/ScheduleEventCalendar/EventViewer.cs
+++ Homeworks/ScheduleEventCalendar/EventViewer.cs
@@ -16,9 +16,16 @@ namespace ScheduleEventCalendar
         {
             InitializeComponent();
 
-            monthCalendar.MouseDown += MonthCalendar_MouseDown;
+            LoadEventsForDate(monthCalendar.TodayDate);
 
+            monthCalendar.AllowDrop = true;
+
+            monthCalendar.MouseDown += MonthCalendar_MouseDown;
             LstBxEvents.MouseDown += LstBxEvents_MouseDown;
+
+            monthCalendar.DragEnter += MonthCalendar_DragEnter;
+            monthCalendar.DragDrop += MonthCalendar_DragDrop;
+            monthCalendar.DragOver += MonthCalendar_DragOver;
         }
 
         private void MonthCalendar_MouseDown(object sender, MouseEventArgs e)
@@ -66,7 +73,7 @@ namespace ScheduleEventCalendar
             LoadEventsForDate(selectedDate);
         }
 
-        private void monthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        private void MonthCalendar_DateChanged(object sender, DateRangeEventArgs e)
         {
             LoadEventsForDate(e.End);
         }
@@ -133,7 +140,79 @@ namespace ScheduleEventCalendar
                 if (index != ListBox.NoMatches)
                 {
                     LstBxEvents.SelectedIndex = index;
-                    contextMenuStripEventList.Show(LstBxEvents, e.Location); 
+                    contextMenuStripEventList.Show(LstBxEvents, e.Location);
+                }
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                int index = LstBxEvents.IndexFromPoint(e.Location);
+                if (index != ListBox.NoMatches)
+                {
+                    LstBxEvents.SelectedIndex = index;
+
+                    if (LstBxEvents.SelectedItem is ScheduleEvent scheduleEvent)
+                    {
+                        // ❗ вызываем у самого списка, не у UserControl
+                        LstBxEvents.DoDragDrop(scheduleEvent, DragDropEffects.Move);
+                    }
+                }
+            }
+        }
+
+        private void MonthCalendar_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(ScheduleEvent)))
+                e.Effect = DragDropEffects.Move;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void MonthCalendar_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(ScheduleEvent)))
+            {
+                Point clientPoint = monthCalendar.PointToClient(new Point(e.X, e.Y));
+
+                var date = GetDateFromPoint(clientPoint);
+
+                if (date.HasValue)
+                {
+                    e.Effect = DragDropEffects.Move;
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+            }
+        }
+
+
+        private void MonthCalendar_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(ScheduleEvent)))
+            {
+                var movedEvent = (ScheduleEvent)e.Data.GetData(typeof(ScheduleEvent));
+
+                Point clientPoint = monthCalendar.PointToClient(new Point(e.X, e.Y));
+                var hit = monthCalendar.HitTest(clientPoint);
+
+                if (hit.HitArea == MonthCalendar.HitArea.Date)
+                {
+                    DateTime newDate = hit.Time.Date;
+
+                    // Удаляем из старой даты
+                    if (_events.ContainsKey(movedEvent.EventDate))
+                        _events[movedEvent.EventDate].Remove(movedEvent);
+
+                    // Добавляем в новую дату
+                    movedEvent.EventDate = newDate;
+                    
+                    if (!_events.ContainsKey(newDate))
+                        _events[newDate] = new List<ScheduleEvent>();
+
+                    _events[newDate].Add(movedEvent);
+
+                    LoadEventsForDate(monthCalendar.SelectionStart);
                 }
             }
         }
