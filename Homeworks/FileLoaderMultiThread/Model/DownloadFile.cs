518df474a32c735a12df2e4cc79ad94b6857e939
ValeryKuzh <valery.kuzh@gmail.com>
2025-11-03 17:17:30 +0300

    Create model for downloading file

---
+++ Homeworks/FileLoaderMultiThread/Model/DownloadFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLoaderMultiThread.Model
+{
+    public class DownloadFile
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Url { get; set; }
+        public string FilePathToSave { get; set; }
+        public long TotalSize { get; set; }
+        public long DownloadedSize { get; set; }
+        public bool IsLoaded { get; set; }
+    }
+}
