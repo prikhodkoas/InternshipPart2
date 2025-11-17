9946fcde5c7edc3651ce8281e88fc794c4b845ac
ValeryKuzh <valery.kuzh@gmail.com>
2025-11-17 15:52:12 +0300

    Update Service

---
+++ Homeworks/XMLNotes/Service/NoteService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XMLNotes.Model;
+
+namespace XMLNotes
+{
+    public class NoteService
+    {
+        private readonly NoteRepository _repository;
+
+        public NoteService(NoteRepository repository)
+        {
+            _repository = repository;
+            _repository.Create();
+        }
+
+        public List<Note> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Add(Note note)
+        {
+            _repository.Add(note);
+        }
+
+        public void Update(Note note)
+        {
+            _repository.Update(note);
+        }
+
+        public void Delete(Guid id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
