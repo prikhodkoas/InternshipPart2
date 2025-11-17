0339be4e9d7b37e9d7f0d5c795f994b034a02910
ValeryKuzh <valery.kuzh@gmail.com>
2025-11-17 12:49:27 +0300

    Create proj for XML Notes

---
+++ Homeworks/XMLNotes/Model/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLNotes.Model
+{
+    /// <summary>
+    /// Модель заметки
+    /// </summary>
+    public class Note
+    {
+
+    }
+}
