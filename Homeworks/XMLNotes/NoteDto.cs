be82cc790e3bcf82d3e488377086eb951b4937cb
ValeryKuzh <valery.kuzh@gmail.com>
2025-11-21 11:58:26 +0300

    Add NoteDto

---
+++ Homeworks/XMLNotes/NoteDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Remoting.Activation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLNotes
+{
+    public class NoteDto
+    {
+        [Browsable(false)]
+        public string Id {  get; set; }
+
+        [Browsable(true)]
+        [DisplayName("Название задачи")]
+        public string Title { get; set; }
+
+        [Browsable(true)]
+        [DisplayName("Описание задачи")]
+        public string Description { get; set; }
+        
+        [Browsable(true)]
+        [DisplayName("Последнее обновление")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
