b86d628b1f7b7345450844ab79f9963d03b4c352
ValeryKuzh <valery.kuzh@gmail.com>
2025-11-22 23:26:12 +0300

    Update NoteDto

--- Homeworks/XMLNotes/UIModel/NoteDto.cs
+++ Homeworks/XMLNotes/UIModel/NoteDto.cs
@@ -11,22 +11,59 @@ namespace XMLNotes
     public class NoteDto : INotifyPropertyChanged
     {
         [Browsable(false)]
-        public string Id {  get; set; }
+        public string Id { get; set; }
 
+        private string _title;
         [Browsable(true)]
         [DisplayName("Название задачи")]
-        public string Title { get; set; }
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged(nameof(Title));
+                }
+            }
+        }
 
+        private string _description;
         [Browsable(true)]
         [DisplayName("Описание задачи")]
-        public string Description { get; set; }
-        
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
+            }
+        }
+
+        private DateTime _updatedAt;
         [Browsable(true)]
         [DisplayName("Последнее обновление")]
-        public DateTime UpdatedAt { get; set; }
+        public DateTime UpdatedAt
+        {
+            get => _updatedAt;
+            set
+            {
+                if (_updatedAt != value)
+                {
+                    _updatedAt = value;
+                    OnPropertyChanged(nameof(UpdatedAt));
+                }
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
